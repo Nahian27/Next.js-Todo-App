eeab37294938c674be3fd9bd9c8f746b318151f3
Nahian27 <alnpulok27@gmail.com>
2023-02-07 16:31:41 +0600

    Update axios.js

--- src/lib/axios.js
+++ src/lib/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 export default axios.create({
-    baseURL: 'https://nahian-api.000webhostapp.com/api/todos',
+    baseURL: process.env.API_URL,
     headers: {
         'X-Requested-With': 'XMLHttpRequest',
     },

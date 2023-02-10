d93bf3c1313cabacae3c9ade5600793d6a9b5c3e
Nahian27 <alnpulok27@gmail.com>
2023-02-10 15:09:22 +0600

    Delete axios.ts

--- src/lib/axios.ts
+++
@@ -1,9 +0,0 @@
-import Axios from 'axios'
-
-export default Axios.create({
-    baseURL: process.env.NEXT_PUBLIC_API_URL,
-    headers: {
-        'X-Requested-With': 'XMLHttpRequest',
-    },
-    withCredentials: false
-})
